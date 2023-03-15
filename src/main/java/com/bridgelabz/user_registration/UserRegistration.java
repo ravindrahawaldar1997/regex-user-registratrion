0f8b113a1244f901488961c700718295df1e6b02
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-03-15 19:54:13 +0530

    Added: program for valid password with minimum eight characters

--- src/main/java/com/bridgelabz/user_registration/UserRegistration.java
+++ src/main/java/com/bridgelabz/user_registration/UserRegistration.java
@@ -3,9 +3,10 @@ package com.bridgelabz.user_registration;
 public class UserRegistration {
     public static void main(String[] args) {
         ValidateUserInfo validateUserInfo = new ValidateUserInfo();
-//        validateUserInfo.firstName();
-//        validateUserInfo.lastName();
-//        validateUserInfo.email();
+        validateUserInfo.firstName();
+        validateUserInfo.lastName();
+        validateUserInfo.email();
         validateUserInfo.mobileNumber();
+        validateUserInfo.passwordWithEightChar();
     }
 }
