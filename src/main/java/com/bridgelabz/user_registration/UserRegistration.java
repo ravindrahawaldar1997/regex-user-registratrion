4e3544258042a02755ade3a5fcb6149de4d0ad84
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-03-15 12:26:58 +0530

    Added: program for valid first name

---
+++ src/main/java/com/bridgelabz/user_registration/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.user_registration;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        ValidateUserInfo validateUserInfo = new ValidateUserInfo();
+        validateUserInfo.firstName();
+    }
+}
---
+++ src/main/java/com/bridgelabz/user_registration/ValidateUserInfo.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.user_registration;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateUserInfo {
+    Scanner sc = new Scanner(System.in);
+
+    public void firstName() {
+        System.out.println("Enter the first name ");
+        String firstName = sc.next();
+
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+        Matcher matcher = pattern.matcher(firstName);
+        boolean matchFound = matcher.find();
+        if (matchFound) {
+            System.out.println("first name is valid");
+        } else {
+            System.out.println("first name is invalid");
+        }
+    }
+}
